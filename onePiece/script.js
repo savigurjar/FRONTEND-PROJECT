80c90f360ff2d363dd18b1219673dddc1c8d52c0
Savita <96195601+savigurjar@users.noreply.github.com>
2024-09-27 09:55:44 +0530

    Add files via upload

---
+++ onePiece/script.js
@@ -0,0 +1,117 @@
+
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x + 30+ "px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x - 250 +"px"
+    blur.style.top = dets.y - 250 +"px"
+})
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  height: "110px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers: true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers:true,
+    start:"top -25%",
+    end:"top -70%",
+    scrub:2,
+  },
+});
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 3;
+        crsr.style.border = "0.1px solid #fff";  // Thicker border
+        crsr.style.backgroundColor = "transparent";
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #A10009";  // Thicker border
+        crsr.style.backgroundColor = "#A10009";
+    })
+});
+
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+     
+     scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 58%",
+        scrub:3
+     } 
+})
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+     
+     scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:2
+     } 
+})
+
+gsap.from("#coma1",{
+y:-70,
+x:-70,
+scrollTrigger:{
+    trigger:"#coma1",
+    scroller:"body",
+    // markers:true,
+    start:"top 55%",
+    end:"top 45%",
+    scrub:4
+}
+});
+
+gsap.from("#coma2",{
+y:70,
+x:70,
+scrollTrigger:{
+    trigger:"#coma2",
+    scroller:"body",
+    // markers:true,
+    start:"top 55%",
+    end:"top 45%",
+    scrub:4
+}
+});
+
+gsap.from("#page4 h1",{
+y:50,
+scrollTrigger:{
+    trigger:"page4 h1",
+    scroller:"body",
+    // markers:true,
+    start:"top 75%",
+    end:"top 70%",
+    scrub:3
+}
+})
